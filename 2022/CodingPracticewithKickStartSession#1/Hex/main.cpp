34ab6422d087b0db956deb4a86dba0fd5ab04e0b
polar.zhang <zhyuanshan@gmail.com>
2022-05-15 19:43:49 +0800

    A n^2 solution, tracing the paths from North-west to the south/east and South-west to north/east.

--- 2022/CodingPracticewithKickStartSession#1/Hex/main.cpp
+++ 2022/CodingPracticewithKickStartSession#1/Hex/main.cpp
@@ -1,128 +1,138 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// DFS traverse, visit all cells.
-void markPath(const vector<vector<char>>& board, vector<vector<bool>>& visited, int x, int y, char chess){
-    int n = board.size();
-    if(x < 0 || x >= n || y < 0 || y >= n || visited[x][y] || board[x][y] != chess) return;
-    visited[x][y] = true;
+vector<pair<int, int>> directions = {{0, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 0}, {1, -1}}; // From 0 -> size(), circle from the left clockwise.
 
-    // Check top left
-    markPath(board, visited, x - 1, y, chess);
-    // Check top right
-    markPath(board, visited, x - 1, y + 1, chess);
-    // Check left.
-    markPath(board, visited, x, y - 1, chess);
-    // check right.
-    markPath(board, visited, x, y + 1, chess);
-    // Check bottom-left.
-    markPath(board, visited, x + 1, y - 1, chess);
-    // check bottom.
-    markPath(board, visited, x + 1, y, chess);
+bool tracePathFromNorthWest(const vector<vector<char>>& colorBoard, unordered_set<int> & pathHash){
+    int n = colorBoard.size();
+    int x = 0, y = 0, next = 4; // Start from 0, 0, the next element is the index of directions, directions[4] = {1, 0}
+    while(x < n && y < n) { // Either x or y will reach to the other end.
+        pathHash.insert(x * 1000 + y); // Use hash to get interception faster.
+        int nextX = x + directions[next].first;
+        int nextY = y + directions[next].second;
+
+        if(colorBoard[nextX][nextY] == 'C') {
+            x = nextX;
+            y = nextY;
+            next = (next - 1 + directions.size()) % directions.size(); // direction changed anti-clockwise
+
+            if(y >= n - 1) return true;
+            if(x >= n - 1 && y == 0) return false;
+        }
+        else {
+            next = (next + 1) % directions.size(); // direction changed clockwise
+        }
+    }
+
+    return y >= n - 1;
 }
 
-bool checkRedWins(const vector<vector<char>>& board){
-    int n = board.size();
-    vector<vector<bool>> visited(n, vector<bool>(n, false));
-    for(int i = 0; i < n; i++){
-        markPath(board, visited, 0, i, 'R');
+bool tracePathFromSouthWest(const vector<vector<char>>& colorBoard, unordered_set<int> & pathHash){
+    int n = colorBoard.size();
+    int x = n - 1, y = 0, next = 2; // Start from n -1, 0, the next element is the index of directions, directions[2] = {-1, 1}
+    while(x >= 0 && y < n) { // Either x or y will reach to the other end.
+        pathHash.insert(x * 1000 + y); // Use hash to get interception faster.
+        int nextX = x + directions[next].first;
+        int nextY = y + directions[next].second;
+
+        if(colorBoard[nextX][nextY] == 'C') {
+            x = nextX;
+            y = nextY;
+            next = (next + 1) % directions.size(); // direction changed clockwise
+
+            if(y >= n - 1) return true;
+            if(x <= 0 && y == 0) return false;
+        }
+        else {
+            next = (next - 1 + directions.size()) % directions.size(); // direction changed anti-clockwise
+        }
     }
 
-    for(int i = 0; i < n; i++){
-        if(visited[n - 1][i]) return true; // End of the board visited.
-    }
-
-    return false;
+    return y >= n - 1;
 }
 
-bool checkBlueWins(const vector<vector<char>>& board){
-    int n = board.size();
-    vector<vector<bool>> visited(n, vector<bool>(n, false));
-    for(int i = 0; i < n; i++){
-        markPath(board, visited, i, 0, 'B');
+enum class Status{Impossible, Win, NotWin};
+Status checkGameStatus(const vector<vector<char>>& colorBoard){
+    int n = colorBoard.size();
+    unordered_set<int> topPath;
+    if(tracePathFromNorthWest(colorBoard, topPath)){
+        unordered_set<int> bottomPath;
+        tracePathFromSouthWest(colorBoard, bottomPath);
+
+        for(int n : topPath){
+            if(bottomPath.find(n) != bottomPath.end()){
+                return Status::Win;// The two paths intercepted, means this is the only way to win.
+            }
+        }
+
+        return Status::Impossible; // No interception, meaning deleting any chess won't break the win.
     }
 
-    for(int i = 0; i < n; i++){
-        if(visited[i][n - 1]) return true; // End of the board visited.
-    }
-
-    return false;
+    return Status::NotWin;
 }
 
-string FindBoardStatus(vector<vector<char>>& board) {
+string FindBoardStatus(const vector<vector<char>>& board) {
     int n = board.size();
     int redCount = 0, blueCount = 0;
-    for(auto& row : board){
-      for(char c : row){
-          if(c == 'R') redCount ++;
-          else if(c == 'B') blueCount ++;
-      }
-    }
-    
-    if(abs(redCount - blueCount) > 1) return "Impossible";
-    
-    if(checkRedWins(board)){
-        if(blueCount > redCount){ // Blue count is bigger, means blue is last step and Red could not win.
-            return "Impossible";
-        }
-
-        // Check whether Red is double win: removing one red would break the win.
-        for(int k = 0;k < n; k++){
-            for(int l = 0;l < n; l ++){
-                if(board[k][l] == 'R'){
-                    board[k][l] = '.'; // Remove one Red.
-                    bool redWins = !checkRedWins(board);
-                    board[k][l] = 'R'; // Restore Red
-
-                    if(redWins){ // The cross is broken, means Red wins only once.
-                        return "Red wins";
-                    }
+    // Extend the board by 1 on each side, in order to reduce the code lines, we put the different color on separate board.
+    vector<vector<char>> blueBoard(n + 2, vector<char>(n + 2, '.'));
+    vector<vector<char>> redBoard(n + 2, vector<char>(n + 2, '.'));
+    for(int i = 0;i < n + 2; i ++){
+        for(int j = 0;j < n + 2; j ++){
+            if(j == 0 || j == n + 1){// First column and last column is the color we need to check, so we can always go through the column.
+                blueBoard[i][j] = 'C';
+                redBoard[i][j] = 'C';
+            }
+            else if(i != 0 && i != n + 1) { // The actual board is in the middle.
+                if(board[i - 1][j - 1] == 'B'){
+                    blueBoard[i][j] = 'C'; // Use the same character for Blue & Red, so that we can use the same code to check it.
+                    blueCount ++;
+                }
+                
+                if(board[j - 1][i - 1] == 'R'){ // Rotate the Red for easier checking.
+                    redBoard[i][j] = 'C'; // Use the same character for Blue & Red, so that we can use the same code to check it.
+                    redCount ++;
                 }
             }
         }
-
-        return "Impossible"; // Couldn't break the cross by removing one Red, this means double wining.
     }
+
+    if(abs(redCount - blueCount) > 1) return "Impossible"; // One color has two more steps.
+
+    Status blueStatus = checkGameStatus(blueBoard);
+    if(blueStatus == Status::Win) {
+        if(blueCount >= redCount) return "Blue wins"; // Only equal steps or bigger steps can this color wins.
+        else return "Impossible";
+    }
+    else if(blueStatus == Status::Impossible) return "Impossible";
     
-    if(checkBlueWins(board)){
-        if(redCount > blueCount){ // Red count is bigger, means red is last step and blue could not win.
-            return "Impossible";
-        }
-
-        // Check whether Blue is double win: removing one blue would break the win.
-        for(int k = 0;k < n; k++){
-            for(int l = 0;l < n; l ++){
-                if(board[k][l] == 'B'){
-                    board[k][l] = '.'; // Remove one Blue.
-                    bool redWins = !checkBlueWins(board);
-                    board[k][l] = 'B'; // Restore Blue
-
-                    if(redWins){ // The cross is broken, means Blue wins only once.
-                        return "Blue wins";
-                    }
-                }
-            }
-        }
-
-        return "Impossible"; // Couldn't break the cross by removing one Blue, this means double wining.
+    Status redStatus = checkGameStatus(redBoard);
+    if(redStatus == Status::Win) {
+        if(redCount >= blueCount) return "Red wins";  // Only equal steps or bigger steps can this color wins.
+        else return "Impossible";
     }
+    else if(redStatus == Status::Impossible) return "Impossible";
 
     return "Nobody wins";
 }
 
 int main() {
+    ifstream fin("ts2_input.txt");
+    ofstream fout("output.txt");
+    // auto& fin = cin;
+    // auto& fout = cout;
   int t;
-  cin >> t;
+  fin >> t;
   for (int tc = 1; tc <= t; ++tc) {
     int n;
-    cin >> n;
+    fin >> n;
     vector<vector<char>> board(n, vector<char>(n));
     for (int r = 0; r < n; ++r) {
       for (int c = 0; c < n; ++c) {
-        cin >> board[r][c];
+        fin >> board[r][c];
       }
     }
-    cout << "Case #" << tc << ": " << FindBoardStatus(board) << endl;
+    fout << "Case #" << tc << ": " << FindBoardStatus(board) << endl;
   }
   return 0;
 }
